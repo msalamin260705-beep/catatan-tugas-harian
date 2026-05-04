ae129b1c38aed117dc136a937e2d6fbb39983a36
msalamin260705-beep <msalamin260705@gmail.com>
2026-05-04 14:02:26 +0700

    init

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,26 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "db_tugas_mahasiswa");
 
-// Cek apakah data identitas sudah diisi
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_tugas_mahasiswa";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi ke database gagal: " . mysqli_connect_error());
+}
+
+if (!file_exists('uploads')) {
+    mkdir('uploads', 0777, true);
+}
+
 $check = mysqli_query($conn, "SELECT * FROM identitas LIMIT 1");
 $user_exists = mysqli_num_rows($check) > 0;
 
-if (!$user_exists && basename($_SERVER['PHP_SELF']) != 'identitas.php' && basename($_SERVER['PHP_SELF']) != 'simpan.php') {
+$current_page = basename($_SERVER['PHP_SELF']);
+if (!$user_exists && $current_page != 'identitas.php' && $current_page != 'simpan.php') {
     header("Location: identitas.php");
     exit();
 }
